c121b21c2adbd5360813d8599e0959293376c82e
vrajdesai78 <vrajdesai78@gmail.com>
2024-04-21 20:49:13 +0530

    updated image

--- app/mint/route.tsx
+++ app/mint/route.tsx
@@ -13,35 +13,9 @@ const handleRequest = frames(async (ctx) => {
   const frameMessage = await getFrameMessage(await ctx.request.json());
   const { requesterVerifiedAddresses } = frameMessage;
 
-  if (requesterVerifiedAddresses && address) {
-    const hash = await mintNFT(address, requesterVerifiedAddresses[0]);
-    console.log("mint hash", hash);
-  }
-
   return {
-    image: (
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-          padding: "20px",
-          backgroundColor: "whitesmoke",
-          width: "100%",
-          height: "100%",
-        }}
-      >
-        <h1
-          style={{
-            fontSize: "4rem",
-            marginBottom: "10px",
-          }}
-        >
-          You have minted successfully!
-        </h1>
-      </div>
-    ),
+    image:
+      "https://telehealth-chat-files.s3.ap-south-1.amazonaws.com/3bd27784-29c0-4711-9e12-b91f5b488a8d",
     buttons: [
       <Button
         action='tx'
